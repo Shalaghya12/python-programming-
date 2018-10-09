f4cab39778256a540fdce71e16a3a0e2f01c5eaf
Shalaghya12 <43983058+Shalaghya12@users.noreply.github.com>
2018-10-09 14:01:55 +0530

    Add files via upload

--- lab_function.py
+++ lab_function.py
@@ -1,13 +1,12 @@
 def ArmN(x):
-	sum=0			   	#Sum initilized to zero
-	a=x				#Value given to a variable			
-	while (a>0):			#while loop is used to check the condition
-		d=a%10			#modulus taken of the no,
-		sum+=d**3		#sum is sum+cube of no
-		a=a//10			#then again it is divided to get the quotient
+	sum=0			   
+	a=x							
+	while (a>0):		
+		d=a%10
+		sum+=d**3		
+		a=a//10			
 	if sum==x:
-		return 'Armstrong no'
+		print( 'Armstrong no')
 	else:
-		return 'not armstrong no'
-x=int(input("enter the no"))
-print(ArmN(x))		
+		print( 'not armstrong no')
+
